45fddb358c87d0ab5d0245021701d3fefb629d7e
Illia <ikuchko@gmail.com>
2016-05-09 11:20:10 -0700

    remove files listed in .gitignore
    Former-commit-id: f53e4b5e038ff8989838cebad049df2e0bd8eea2
    Former-commit-id: 72feafdf6dbc05f1ac889aff06a1d0defb475aff

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,13 +9,20 @@ public class App {
 
 	public static void main(String[] args) {
 		String layout = "templates/layout.vtl";
-		
+		staticFileLocation("/public");
+
 		get("/", (request, response) -> {
-		      HashMap<String, Object> model = new HashMap<String, Object>();
+      HashMap<String, Object> model = new HashMap<String, Object>();
 //		      model.put("users", User.all());
-		      model.put("template", "templates/index.vtl");
-		      return new ModelAndView(model, layout);
-		    }, new VelocityTemplateEngine());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+	  }, new VelocityTemplateEngine());
+
+		get("/transfer", (request, response) -> {
+			HashMap<String, Object> model = new HashMap<String, Object>();
+
+		}, new VelocityTemplateEngine());
+
 	}
 
 }
