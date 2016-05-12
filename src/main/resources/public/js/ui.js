0785e60540045ca4e645bfb0c60fa31c42ffd216
Illia <ikuchko@gmail.com>
2016-05-12 16:51:19 -0700

    adjust an image to fit a column grid

--- src/main/resources/public/js/ui.js
+++ src/main/resources/public/js/ui.js
@@ -4,73 +4,20 @@ $(function() {
     verifyUser($('#login').val(), $('#password').val(), event);
   });
 
-  // $.get( "/tiff", function( data ) {
-  //   var parentEl = $(this).parent();
-  //   alert("Response here: " + data);
-  //   var b64Response = btoa(data);
-  //
-  //   // create an image
-  //   var outputImg = document.createElement('img');
-  //   outputImg.src = 'data:image/png;base64,'+b64Response;
-  //
-  //   // append it to your page
-  //   document.body.appendChild(outputImg);
-  //
-  //   // var filrR = new FileReader();
-  //   // filrR.onload = function(e) {
-  //   //   $(parentEl).append('<img src="' + filrR.result + '" class="preview"/>');
-  //   // };
-  //   // filrR.readAsDataURL(data);
-  //
-  //   // $(parentEl).append('<img src="' + fr.result + '" class="preview"/>');
-  // });
-
   var xhr = new XMLHttpRequest();
+  console.log("Start loading");
   xhr.responseType = 'arraybuffer';
   xhr.open('GET', "/2016021200.001");
   xhr.onload = function ( e ) {
     Tiff.initialize({TOTAL_MEMORY: 133554432 })
      var tiff = new Tiff({buffer: xhr.response});
      var canvas = tiff.toCanvas();
-     console.log(canvas);
-     document.body.appendChild(canvas);
+     $(canvas).addClass("tiff-image");
+     $(".scrollbox").append(canvas);
+     console.log("End loading");
   };
   xhr.send();
 
-
-  // $.get( "/tiff", function( data ) {
-  //   var parentEl = $(this).parent();
-  //   alert(data.header);
-  //   var fr = new FileReader();
-  //   fr.onload = function(e) {
-  //     //Using tiff.min.js library - https://github.com/seikichi/tiff.js/tree/master
-  //     console.debug("Parsing TIFF image...");
-  //     //initialize with 100MB for large files
-  //     Tiff.initialize({
-  //       TOTAL_MEMORY: 100000000
-  //     });
-  //     var tiff = new Tiff({
-  //       buffer: e.target.result
-  //     });
-  //     var tiffCanvas = tiff.toCanvas();
-  //     $(tiffCanvas).css({
-  //       "max-width": "1000000px",
-  //       "width": "100%",
-  //       "height": "auto",
-  //       "display": "block",
-  //       "padding-top": "10px"
-  //     }).addClass("preview");
-  //     $(parentEl).append(tiffCanvas);
-  //   }
-  //
-  //   fr.onloadend = function(e) {
-  //     console.debug("Load End");
-  //   }
-  //   var blob = new Blob([data], {type: 'image/tiff'});
-  //   var arrayBuffer = this.response;
-  //      var file = new File({buffer: arrayBuffer}, "123.tif");
-  //     fr.readAsArrayBuffer(file);
-  // });
 
   var fileTypes = ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'pdf']; //acceptable file types
   $("input:file").change(function(evt) {
