2c6e72c9316dda158edf18350792636e7a4960f5
Illia <ikuchko@gmail.com>
2016-05-13 12:16:35 -0700

    create class for extractiong files

--- src/main/java/ArchiveReader.java
+++ src/main/java/ArchiveReader.java
@@ -1,8 +1,27 @@
+import net.lingala.zip4j.exception.ZipException;
 
+import java.io.File;
+
+import net.lingala.zip4j.core.ZipFile;
 
 public class ArchiveReader {
+	private static final String SOURCE_DIRECTORY = "src/main/resources/temp/FTPInput/";
+	private static final String DESTINATION_DIRECTORY = "src/main/resources/public/temp/";
 	
-	
-	
+	public static Boolean extractFiles (String sourceFile, Session session) {
+		String userLogin = session.getFTPUserLogin();
+		String source = SOURCE_DIRECTORY + userLogin + "/" + sourceFile;
+		String destination = DESTINATION_DIRECTORY + session.getFTPUserLogin() + "/" + sourceFile;
+		File tempFolder = new File(destination);
+		tempFolder.mkdirs();
+		try {
+	         ZipFile zipFile = new ZipFile(source);
+	         zipFile.extractAll(destination);
+	    } catch (ZipException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+		return true;
+	}
 }
 
--- src/test/java/ArchiveReaderTest.java
+++ src/test/java/ArchiveReaderTest.java
@@ -8,5 +8,9 @@ import org.junit.Test;
 
 public class ArchiveReaderTest {
 	
-	
+	@Test
+	public void zip_extracteFilesReturnTrue() {
+		Session session = new Session("lps_nort", "empty");
+		assertTrue(ArchiveReader.extractFiles("NHA-20160425.zip", session));
+	}
 }
